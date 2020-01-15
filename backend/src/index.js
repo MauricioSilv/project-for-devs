fa12326be6080ac95e21ebf84637986beac23a09
mauriciosilv <mausilva828@gmail.com>
2020-01-14 21:33:04 -0300

    Added for backend project devs

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack10:maumau@cluster0-ij5mp.mongodb.net/week10?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use(routes);
+
+
+
+
+app.listen(3333);
