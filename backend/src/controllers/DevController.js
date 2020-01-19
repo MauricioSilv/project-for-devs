bc17c265f3ae04e2acf2401b7ec512fccd306779
mauriciosilv <mausilva828@gmail.com>
2020-01-19 15:39:37 -0300

    adding socket.io settings

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket');
 
 module.exports = {
   async index(_, res){
@@ -8,8 +9,6 @@ module.exports = {
 
     return res.json(devs);
   },
-
-
 
   async store(req, res) {
     const { github_username, techs, latitude, longitude } = req.body;
@@ -34,6 +33,13 @@ module.exports = {
         techs: techArray,
         location
       });
+
+      const sendSocketMessageTo = findConnections(
+        { latitude, longitude },
+        techArray,
+      )
+
+      sendMessage(sendSocketMessageTo, 'new-dev', dev);
     }
     return res.json(dev);
   }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('./routes');
 const cors = require('cors');
+const http = require('http');
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
+
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://omnistack10:maumau@cluster0-ij5mp.mongodb.net/week10?retryWrites=true&w=majority', {
   useNewUrlParser: true,
@@ -13,7 +19,4 @@ app.use(cors())
 app.use(express.json());
 app.use(routes);
 
-
-
-
-app.listen(3333);
+server.listen(3333);
